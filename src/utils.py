8bbb9c390b094fa53f0d79e3f6b93f00008e049f
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2024-09-28 05:27:14 -0700

    Add model and tokenizer loading from utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,17 @@
 import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+def load_model_and_tokenizer(model_name, dtype, hf_token, device):
+    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=dtype, trust_remote_code=True, token=hf_token).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, token=hf_token, trust_remote_code=True)
+
+    tokenizer.pad_token = tokenizer.eos_token
+    if isinstance(model.generation_config.eos_token_id, list):
+        model.generation_config.pad_token_id = model.generation_config.eos_token_id[0]
+    else:
+        model.generation_config.pad_token_id = model.generation_config.eos_token_id
+
+    return model, tokenizer
 
 def sampling_with_temperature(logits, temperature=1.0):
     probs = torch.softmax(logits / (temperature + 1e-10), dim=-1)
