e7c0c49c5160b6a75d8a905545f8052cfe5b44a3
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2024-09-28 23:31:26 -0700

    Apply temperature scaling first - as in the GPT2 and HF implementations

--- src/top_p.py
+++ src/top_p.py
@@ -1,6 +1,10 @@
 import torch
 
 def top_p_sampling_with_temperature(logits, top_p, temperature=1.0):
+    # Temperature scaling first
+    logits = logits / (temperature + 1e-10)
+    
+    # Top-p filtering
     sorted_logits, sorted_indices = torch.sort(logits, dim=-1, descending=True)
     sorted_probs = torch.softmax(sorted_logits, dim=-1)
     cumsum_sorted_probs = torch.cumsum(sorted_probs, dim=-1)
@@ -10,7 +14,6 @@ def top_p_sampling_with_temperature(logits, top_p, temperature=1.0):
     top_p_mask[..., 0] = 1
 
     top_p_logits = sorted_logits.masked_fill(~top_p_mask, float('-inf'))
-    top_p_logits = top_p_logits / (temperature + 1e-10)
     top_p_probs = torch.softmax(top_p_logits, dim=-1)
     
     sample_index = torch.multinomial(top_p_probs, num_samples=1)
