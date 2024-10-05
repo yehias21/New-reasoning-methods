67bc4314ebe269ddbdc6f945a37276020773e73d
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2024-10-04 22:34:53 -0700

    Do masking instead of filtering + add comparison test with HF

--- src/top_k.py
+++ src/top_k.py
@@ -1,19 +1,26 @@
 import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, TopKLogitsWarper
 
-def top_k_sampling_with_temperature(logits, top_k=50, temperature=1.0):
+def top_k_sampling_with_temperature(logits, top_k=50, temperature=1.0, return_logits=False):
     # Temperature scaling first
     logits = logits / (temperature + 1e-10)
     
-    # Top-k transformation
     if top_k == 0 or top_k > logits.shape[-1]:
         top_k = logits.shape[-1]
 
+    # Top-k transformation
     top_k_logits, top_k_indices = torch.topk(logits, k=top_k, dim=-1)
     top_k_probs = torch.softmax(top_k_logits, dim=-1)
-    
+
+    indices_to_remove = logits < top_k_logits[:, -1]
+    masked_logits = logits.masked_fill(indices_to_remove, float('-inf'))
+    masked_probs = torch.softmax(masked_logits, dim=-1)
+
     # Sampling
-    sample_index = torch.multinomial(top_k_probs, num_samples=1)
-    sample_token = top_k_indices.gather(-1, sample_index)[0]
+    sample_token = torch.multinomial(masked_probs, num_samples=1)[0]
+
+    if return_logits:
+        return sample_token, masked_logits
     return sample_token
 
 def generate_with_top_k_sampling(model, tokenizer, device, prompt, max_new_tokens, top_k=10, temperature=1.0):
@@ -50,3 +57,24 @@ def generate_with_top_k_sampling(model, tokenizer, device, prompt, max_new_token
     output = tokenizer.decode(fin_prompt_new_seq[0], skip_special_tokens=True)
     
     return output
+
+def test_top_k_sampling():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = AutoModelForCausalLM.from_pretrained("gpt2").to(device)
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    prompt = "Once upon a time"
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+
+    logits = model(input_ids).logits[:, -1, :]
+    print(logits)
+
+    sample_token, top_k_logits = top_k_sampling_with_temperature(logits, top_k=500, temperature=1.0, return_logits=True)
+    print(top_k_logits)
+    logit_processor = TopKLogitsWarper(top_k=500, min_tokens_to_keep=1)
+    logits_processed = logit_processor(input_ids, logits)
+    print(logits_processed)
+
+    torch.testing.assert_close(top_k_logits, logits_processed)
+    
+if __name__ == "__main__":
+    test_top_k_sampling()
