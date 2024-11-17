5d9629d7363aa71fe038af87a93c1915ca516ba8
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2024-11-17 04:31:03 -0800

    Add medusa to main generation script

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import ast
 import torch
 import numpy as np
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -14,6 +15,7 @@ from src.beam_search import generate_with_beam_search
 from src.cot_decoding import generate_with_cot_decoding
 from src.constrained_json_decoding import constrained_json_sampling
 from src.speculative import speculative_sampling
+from src.medusa import generate_with_medusa
 from src.utils import *
 from src.generation_utils import *
 import json
@@ -23,9 +25,10 @@ torch.set_float32_matmul_precision('high')
 
 def main():
     parser = argparse.ArgumentParser(description="Generate text using a language model.")
-    parser.add_argument("--method", type=str, choices=["unconstrained", "top_k", "top_p", "min_p", "typical", "eta", "epsilon", "beam_search", "cot_decoding", "constrained_json", "speculative"], default="unconstrained", help="Sampling method to use.")
+    parser.add_argument("--method", type=str, choices=["unconstrained", "top_k", "top_p", "min_p", "typical", "eta", "epsilon", "beam_search", "cot_decoding", "constrained_json", "speculative", "medusa"], default="unconstrained", help="Sampling method to use.")
     parser.add_argument("--model", type=str, required=True, help="Path/name of the model.")
     parser.add_argument("--draft-model", type=str, default=None, help="Path/name of the draft model (required for speculative decoding).")
+    parser.add_argument("--medusa-model-heads", type=str, default=None, help="Path/name of the medusa model heads (required for medusa decoding).")
     parser.add_argument("--prompt", type=str, default=None, help="Input sequence for the model.")
     parser.add_argument("--prompt_file", type=str, default=None, help="Path to the file containing the prompt.")
     parser.add_argument("--apply-chat-template", type=str, action=argparse.BooleanOptionalAction, default=False, help="Whether to apply the chat template to the prompt.")
@@ -41,6 +44,7 @@ def main():
     parser.add_argument("--max_number_tokens", type=int, default=6, help="Maximum number of tokens for numbers in constrained JSON sampling.")
     parser.add_argument("--max_string_token_length", type=int, default=10, help="Maximum number of tokens for strings in constrained JSON sampling.")
     parser.add_argument("--lookahead", type=int, default=4, help="Lookahead for speculative decoding.")
+    parser.add_argument("--medusa_choices", type=str, default=None, help="Path to the file containing the medusa choices.")
     parser.add_argument("--min_tokens_to_keep", type=int, default=1, help="Minimum number of tokens to keep when sampling.")
     parser.add_argument("--max_new_tokens", type=int, default=500, help="Maximum number of new tokens to generate.")
     parser.add_argument("--num_return_sequences", type=int, default=1, help="Number of sequences to return.")
@@ -71,22 +75,26 @@ def main():
         with open(args.prompt_file, "r") as f:
             args.prompt = f.read()
 
-    # For Llama pre-trained only models
-    if "Llama" in args.model and args.prompt and not args.apply_chat_template:
-        args.prompt = "<|begin_of_text|>" + args.prompt
-
     # Set device
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    
+
     # Set seed
     if args.seed is not None:
         set_seed(args.seed)
-        
-    # Load model and tokenizer
-    model, tokenizer = load_model_and_tokenizer(args.model, args.dtype, args.hf_token, device)
 
-    if args.apply_chat_template:
-        args.prompt = apply_chat_template(args.prompt, tokenizer)
+    if args.method != "medusa":
+        # Load model and tokenizer
+        model, tokenizer = load_model_and_tokenizer(args.model, args.dtype, args.hf_token, device)
+
+        # For Llama pre-trained only models
+        if "Llama" in args.model and args.prompt and not args.apply_chat_template:
+            args.prompt = "<|begin_of_text|>" + args.prompt
+
+        if args.apply_chat_template:
+            args.prompt = apply_chat_template(args.prompt, tokenizer)
+
+    if args.medusa_choices is not None and args.method == "medusa":
+        args.medusa_choices = ast.literal_eval(args.medusa_choices)
 
     fancy_print("Prompt:", args.prompt)
 
@@ -94,7 +102,7 @@ def main():
     if args.method == "unconstrained":
         sampling_function = unconstrained_sampling_with_temperature
         sampling_params = {"temperature": args.temperature}
-        with torch.no_grad():
+        with torch.inference_mode():
             for _ in range(args.num_return_sequences):
                 output_sequence = generate_with_sampling(model, tokenizer, device, args.prompt, max_new_tokens=args.max_new_tokens, sampling_function=sampling_function, sampling_params=sampling_params)
                 fancy_print("Output:", output_sequence)
@@ -104,7 +112,7 @@ def main():
             parser.error("The --top_k argument is required when using the top-k sampling method.")
         sampling_function = top_k_sampling_with_temperature
         sampling_params = {"top_k": args.top_k, "temperature": args.temperature}
-        with torch.no_grad():
+        with torch.inference_mode():
             for _ in range(args.num_return_sequences):
                 output_sequence = generate_with_sampling(model, tokenizer, device, args.prompt, max_new_tokens=args.max_new_tokens, sampling_function=sampling_function, sampling_params=sampling_params)
                 fancy_print("Output:", output_sequence)
@@ -114,7 +122,7 @@ def main():
             parser.error("The --top_p argument is required when using the top-p sampling method.")
         sampling_function = top_p_sampling_with_temperature
         sampling_params = {"top_p": args.top_p, "temperature": args.temperature}
-        with torch.no_grad():
+        with torch.inference_mode():
             for _ in range(args.num_return_sequences):
                 output_sequence = generate_with_sampling(model, tokenizer, device, args.prompt, max_new_tokens=args.max_new_tokens, sampling_function=sampling_function, sampling_params=sampling_params)
                 fancy_print("Output:", output_sequence)
@@ -124,7 +132,7 @@ def main():
             parser.error("The --min_p argument is required when using the min-p sampling method.")
         sampling_function = min_p_sampling_with_temperature
         sampling_params = {"min_p": args.min_p, "temperature": args.temperature}
-        with torch.no_grad():
+        with torch.inference_mode():
             for _ in range(args.num_return_sequences):
                 output_sequence = generate_with_sampling(model, tokenizer, device, args.prompt, max_new_tokens=args.max_new_tokens, sampling_function=sampling_function, sampling_params=sampling_params)
                 fancy_print("Output:", output_sequence)
@@ -134,7 +142,7 @@ def main():
             parser.error("The --typical_p_mass argument is required when using the typical sampling method.")
         sampling_function = typical_sampling_with_temperature
         sampling_params = {"typical_p_mass": args.typical_p_mass, "temperature": args.temperature}
-        with torch.no_grad():
+        with torch.inference_mode():
             for _ in range(args.num_return_sequences):
                 output_sequence = generate_with_sampling(model, tokenizer, device, args.prompt, max_new_tokens=args.max_new_tokens, sampling_function=sampling_function, sampling_params=sampling_params)
                 fancy_print("Output:", output_sequence)
@@ -144,7 +152,7 @@ def main():
             parser.error("The --epsilon argument is required when using the epsilon sampling method.")
         sampling_function = epsilon_sampling_with_temperature
         sampling_params = {"epsilon": args.epsilon, "temperature": args.temperature}
-        with torch.no_grad():
+        with torch.inference_mode():
             for _ in range(args.num_return_sequences):
                 output_sequence = generate_with_sampling(model, tokenizer, device, args.prompt, max_new_tokens=args.max_new_tokens, sampling_function=sampling_function, sampling_params=sampling_params)
                 fancy_print("Output:", output_sequence)
@@ -154,7 +162,7 @@ def main():
             parser.error("The --epsilon argument is required when using the eta sampling method.")
         sampling_function = eta_sampling_with_temperature
         sampling_params = {"epsilon": args.epsilon, "temperature": args.temperature}
-        with torch.no_grad():
+        with torch.inference_mode():
             for _ in range(args.num_return_sequences):
                 output_sequence = generate_with_sampling(model, tokenizer, device, args.prompt, max_new_tokens=args.max_new_tokens, sampling_function=sampling_function, sampling_params=sampling_params)
                 fancy_print("Output:", output_sequence)
@@ -162,7 +170,7 @@ def main():
     elif args.method == "beam_search":
         if args.beam_width is None:
             parser.error("The --beam_width argument is required when using the beam search method.")
-        with torch.no_grad():
+        with torch.inference_mode():
             for _ in range(args.num_return_sequences):
                 output_sequence = generate_with_beam_search(model, tokenizer, device, args.prompt, max_new_tokens=args.max_new_tokens, beam_width=args.beam_width, temperature=args.temperature)
                 fancy_print("Output:", output_sequence)
@@ -170,7 +178,7 @@ def main():
     elif args.method == "cot_decoding":
         if args.num_return_sequences > 1:
             parser.error("The CoT decoding method only supports single-sequence generation with varying top index for the first token.")
-        with torch.no_grad():
+        with torch.inference_mode():
             for intial_token_k in range(1, 11):
                 output_sequence = generate_with_cot_decoding(model, tokenizer, device, args.prompt, max_new_tokens=args.max_new_tokens, intial_token_k=intial_token_k)
                 if intial_token_k == 1:
@@ -185,7 +193,7 @@ def main():
             parser.error("The temperature should be greater than 0 for constrained JSON sampling.")
         with open(args.json_schema, 'r') as f:
             json_schema = json.load(f)
-        with torch.no_grad():
+        with torch.inference_mode():
             for _ in range(args.num_return_sequences):
                 output_sequence = constrained_json_sampling(
                     model,
@@ -203,7 +211,7 @@ def main():
         if args.draft_model is None:    
             parser.error("The --draft-model argument is required when using the speculative decoding method.")
         else:
-            with torch.no_grad():
+            with torch.inference_mode():
                 for _ in range(args.num_return_sequences):
                     print(f"Using model: {args.model}")
                     print(f"Using draft model: {args.draft_model}")
@@ -213,6 +221,19 @@ def main():
                     output_sequence, acceptance_rate = speculative_sampling(model, draft_model, tokenizer, device, args.prompt, max_new_tokens=args.max_new_tokens, lookahead=args.lookahead, temperature=args.temperature)
                     fancy_print("Output:", output_sequence)
                     fancy_print("Acceptance rate:", acceptance_rate)
+
+    elif args.method == "medusa":
+        if args.medusa_model_heads is None:    
+            parser.error("The --medusa-model-heads argument is required when using the medusa decoding method.")
+        else:
+            with torch.inference_mode():
+                for _ in range(args.num_return_sequences):
+                    print(f"Using model: {args.model}")
+                    print(f"Using draft model: {args.medusa_model_heads}")
+                    if args.medusa_choices is not None:
+                        print(f"Using medusa choices: {args.medusa_choices}")
+                    output_sequence = generate_with_medusa(args.model, args.medusa_model_heads, device, args.prompt, max_new_tokens=args.max_new_tokens, medusa_choices=args.medusa_choices, dtype=args.dtype)
+                    fancy_print("Output:", output_sequence)
     
     else:
         raise NotImplementedError("The specified sampling method is not implemented.")
